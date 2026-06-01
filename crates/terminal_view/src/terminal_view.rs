51196da2042b9a26cb74624f007f53169f5d8b96
miguelraz <miguelraz@ciencias.unam.mx>
2026-06-01 10:28:55 -0600

    mend

--- crates/terminal_view/src/terminal_view.rs
+++ crates/terminal_view/src/terminal_view.rs
@@ -896,7 +896,10 @@ impl TerminalView {
     }
 
     pub fn add_paths_to_terminal(&self, paths: &[PathBuf], window: &mut Window, cx: &mut App) {
-        let mut text = paths.iter().map(|path| format!(" {path:?}")).collect::<String>();
+        let mut text = paths
+            .iter()
+            .map(|path| format!(" {path:?}"))
+            .collect::<String>();
         text.push(' ');
         window.focus(&self.focus_handle(cx), cx);
         self.terminal.update(cx, |terminal, _| {
