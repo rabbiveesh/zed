edc406d07669467a53b792592be27b59ed19533d
miguelraz <miguelraz@ciencias.unam.mx>
2026-06-01 10:10:35 -0600

    use .collect::<String>() instead of .concat()

--- crates/terminal_view/src/terminal_view.rs
+++ crates/terminal_view/src/terminal_view.rs
@@ -897,7 +897,7 @@ impl TerminalView {
     }
 
     pub fn add_paths_to_terminal(&self, paths: &[PathBuf], window: &mut Window, cx: &mut App) {
-        let mut text = paths.iter().map(|path| format!(" {path:?}")).concat();
+        let mut text = paths.iter().map(|path| format!(" {path:?}")).collect::<String>();
         text.push(' ');
         window.focus(&self.focus_handle(cx), cx);
         self.terminal.update(cx, |terminal, _| {
